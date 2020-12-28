300f627aa0a496d41afb5e908af6a59fc0a596c2
coral0209 <leesujung0527@gmail.com>
2020-12-28 11:08:55 +0900

    no message

--- src/main/java/kr/or/ddit/servlet/basic/BasicServlet.java
+++ src/main/java/kr/or/ddit/servlet/basic/BasicServlet.java
@@ -9,10 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BasicServlet extends HttpServlet{
 
 
-	//doXXX 메소드의 인자 : req , res 로 규약 정의되어 있다. 
+	/**
+	 * 
+	 */
+	
+	private static final long serialVersionUID = 1L;
+	private static final Logger logger = LoggerFactory.getLogger(BasicServlet.class);
+
+	
+	
+			//doXXX 메소드의 인자 : req , res 로 규약 정의되어 있다. 
 	//doget , dopost -> httpservlet 에보면 정의가 되있고 재정의 해서 오버라이딩해서 사용. 
 	// GET , POST , DELETE , PUSH , HEAD ... : HTTP 메소드 
 	
@@ -28,7 +40,24 @@ public class BasicServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		
+		logger.debug("BasicServlet.doGet");
+		
+		//만약에 log level 을 debug 보다 높은 레벨로 설정할 경우  logger.debug() 는 출력되지 않지만( 즉 로그를 생성하지는 않지만) 
+		//로그를 생성하지는 않지만 메소드 인자인 문자열 + 문자열 ==> 문자열 결합 연산은 발생한다.
+
+		//문자열 결합이라는 것이 생각보다 큰 퍼포먼스를 일으킨다. -> string buffer 클래스로 바꿔버린다. 
+		//예전 라이브러리에서 이렇게 사용 
+		//if(설정로그레벨 <= debug) {
+			
+		//}
+		
+		//현재 
+		logger.debug("BasicServlet.doGet() userid parameter : " + req.getParameter("userid"));
+		logger.debug("BasicServlet.doGet() userid parameter : {} {} " ,  req.getParameter("userid"),
+				req.getParameter("password"));
 		resp.setContentType("text/html ; charset=utf-8"); //test
+		
 		//printwriter 객체 생성 
 		//재정의
 		//요청을 생성할 때마다 서버의 현재시간이 달라진다.
@@ -52,6 +81,7 @@ public class BasicServlet extends HttpServlet{
 		//localhost/index.jsp ==> servlet 설정에 있는 web.xml 에 등록된 
 		//				*.jsp , *.jspx url-pattern 에 따라 jsp 라는 이름의 서블릿에서 처리 ( JspServlet) 
 		//localhost/doc/20201223.txt 정적인 자료 ==>   
+		
 		
 		
 		PrintWriter pw = resp.getWriter();
